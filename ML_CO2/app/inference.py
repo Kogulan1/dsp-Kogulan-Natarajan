c2b113ab1f65c05d5162d87d08859a95cfe01612
Kogulan1 <65659530+Kogulan1@users.noreply.github.com>
2021-12-18 03:03:23 +0100

    Delete ML_CO2/app directory

--- ML_CO2/app/inference.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from models.app import preprocess
-
-def inference(test_filepath, MODELS_DIR, training_mode):
-    df = pd.read_csv(test_filepath)
-
-    X_Preprocess = preprocess(df, MODELS_DIR, training_mode)
-    return "`Co2 Emission Predicted and saved as Co2_Emission.csv "
-
